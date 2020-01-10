2995bdc5f891b23429473fa40140979c69bc623b
Amy Franz <amy.franz@gmail.com>
2020-01-10 14:21:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point = 0)
   result = starting_point
   i = 0 
   while i < source_array do
-    result = yield(result,source_array[i])
+    result = yield(result, source_array[i])
     i += 1 
   end
   return result
