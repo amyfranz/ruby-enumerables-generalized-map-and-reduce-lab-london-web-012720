8c8c7b605e351e2bbe1fffdd145aace6e341460e
Amy Franz <amy.franz@gmail.com>
2020-01-10 14:29:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,10 +10,10 @@ end
 
 def reduce(source_array, starting_point = nil)
   if starting_point
-    num1 = starting_point
+    result = starting_point
     i = 0
   else
-    num1 = source_array[0]
+    result = source_array[0]
     i = 1 
   end
   while i < source_array.length do
