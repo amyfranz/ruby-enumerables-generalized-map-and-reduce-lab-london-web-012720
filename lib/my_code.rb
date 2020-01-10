fa6a4280e543d26669b24670888bd580dd105482
Amy Franz <amy.franz@gmail.com>
2020-01-10 14:21:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,11 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_point = 0)
+  result = starting_point
+  i = 0 
+  while i < source_array do
+    result = yield(result,source_array[i])
+    i += 1 
+  end
+  return result
+end
